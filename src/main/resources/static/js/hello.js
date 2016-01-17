0abd5ed2f2b19fbc36cc64a759c7feb62c0b55b9
chimney1025 <ttddlplp@gmail.com>
2016-01-17 22:50:14 +0000

    login

--- src/main/resources/static/js/hello.js
+++ src/main/resources/static/js/hello.js
@@ -1,5 +1,4 @@
-angular.module('hello', [ 'ngRoute' ])
-  .config(function($routeProvider, $httpProvider) {
+angular.module('hello', [ 'ngRoute' ]).config(function($routeProvider, $httpProvider) {
 
 	$routeProvider.when('/', {
 		templateUrl : 'home.html',
@@ -9,58 +8,72 @@ angular.module('hello', [ 'ngRoute' ])
 		controller : 'navigation'
 	}).otherwise('/');
 
-    $httpProvider.defaults.headers.common["X-Requested-With"] = 'XMLHttpRequest';
+	$httpProvider.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
-  })
-  .controller('home', function($scope, $http) {
-    $http.get('/resource/').success(function(data) {
-      $scope.greeting = data;
-    })
-  })
-  .controller('navigation',
+}).controller(
+		'navigation',
 
-    function($rootScope, $scope, $http, $location) {
+		function($rootScope, $scope, $http, $location, $route) {
 
-    var authenticate = function(credentials, callback) {
+			$scope.tab = function(route) {
+				return $route.current && route === $route.current.controller;
+			};
 
-      var headers = credentials ? {authorization : "Basic "
-          + btoa(credentials.username + ":" + credentials.password)
-      } : {};
+			var authenticate = function(credentials, callback) {
 
-      $http.get('user', {headers : headers}).success(function(data) {
-        if (data.name) {
-          $rootScope.authenticated = true;
-        } else {
-          $rootScope.authenticated = false;
-        }
-        callback && callback();
-      }).error(function() {
-        $rootScope.authenticated = false;
-        callback && callback();
-      });
+				var headers = credentials ? {
+					authorization : "Basic "
+							+ btoa(credentials.username + ":"
+									+ credentials.password)
+				} : {};
 
-    }
+				$http.get('user', {
+					headers : headers
+				}).success(function(data) {
+					if (data.name) {
+						$rootScope.authenticated = true;
+					} else {
+						$rootScope.authenticated = false;
+					}
+					callback && callback($rootScope.authenticated);
+				}).error(function() {
+					$rootScope.authenticated = false;
+					callback && callback(false);
+				});
 
-    authenticate();
-    $scope.credentials = {};
-    $scope.login = function() {
-        authenticate($scope.credentials, function() {
-          if ($rootScope.authenticated) {
-            $location.path("/");
-            $scope.error = false;
-          } else {
-            $location.path("/login");
-            $scope.error = true;
-          }
-        });
-    };
+			}
 
-    $scope.logout = function() {
-      $http.post('logout', {}).success(function() {
-        $rootScope.authenticated = false;
-        $location.path("/");
-      }).error(function(data) {
-        $rootScope.authenticated = false;
-      });
-    }
-  });
+			authenticate();
+
+			$scope.credentials = {};
+			$scope.login = function() {
+				authenticate($scope.credentials, function(authenticated) {
+					if (authenticated) {
+						console.log("Login succeeded")
+						$location.path("/");
+						$scope.error = false;
+						$rootScope.authenticated = true;
+					} else {
+						console.log("Login failed")
+						$location.path("/login");
+						$scope.error = true;
+						$rootScope.authenticated = false;
+					}
+				})
+			};
+
+			$scope.logout = function() {
+				$http.post('logout', {}).success(function() {
+					$rootScope.authenticated = false;
+					$location.path("/");
+				}).error(function(data) {
+					console.log("Logout failed")
+					$rootScope.authenticated = false;
+				});
+			}
+
+		}).controller('home', function($scope, $http) {
+	$http.get('/resource/').success(function(data) {
+		$scope.greeting = data;
+	})
+});
